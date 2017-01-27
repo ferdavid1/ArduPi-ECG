a03ae085d6057ae54250f0497039c330167a6895
Fernando Espinosa <Fernando Espinosa>
2017-01-27 09:39:22 +0000

    ii first

---
+++ ArduPi.py
@@ -0,0 +1,53 @@
+import serial
+import matplotlib.pyplot as plt
+from drawnow import *
+import atexit
+
+values = []
+
+plt.ion()
+cnt=0
+
+serialArduino = serial.Serial('com3', 115200)
+
+def plotValues():
+    plt.title('Serial value from Arduino')
+    plt.grid(True)
+    plt.ylabel('Values')
+    plt.plot(values, 'rx-', label='values')
+    plt.legend(loc='upper right')
+
+def doAtExit():
+    serialArduino.close()
+    print("Close serial")
+    print("serialArduino.isOpen() = " + str(serialArduino.isOpen()))
+
+atexit.register(doAtExit)
+
+print("serialArduino.isOpen() = " + str(serialArduino.isOpen()))
+
+#pre-load dummy data
+for i in range(0,26):
+    values.append(0)
+    
+while True:
+    while (serialArduino.inWaiting()==0):
+        pass
+    print("readline()")
+    valueRead = serialArduino.readline(500)
+
+    #check if valid value can be casted
+    try:
+        valueInInt = int(valueRead)
+        print(valueInInt)
+        if valueInInt <= 1024:
+            if valueInInt >= 0:
+                values.append(valueInInt)
+                values.pop(0)
+                drawnow(plotValues)
+            else:
+                print("Invalid! negative number")
+        else:
+            print("Invalid! too large")
+    except ValueError:
+        print("Invalid! cannot cast")
